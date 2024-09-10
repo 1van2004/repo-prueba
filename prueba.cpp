2df1748727c2e0fd3cf977e8c72157313ab72fb0
1van2004 <ivan092004@hotmail.com>
2024-09-10 17:38:51 -0600

    Primer commit de prueba

---
+++ prueba.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main(int argc, char const *argv[])
+{
+    int num;
+    cout<<"Ingrese un entero: ";
+    cin>>num;
+    cout<<"Se ingreso el numero "<<num<<"correctamente";
+    return 0;
+}
